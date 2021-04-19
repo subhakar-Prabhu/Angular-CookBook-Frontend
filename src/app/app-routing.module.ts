83f0e06db54ea7fa75c0a042b3bba83091eb7add
subhakar-Prabhu <psubakar@gmail.com>
2021-04-19 14:41:51 +0530

    added routing and other info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { IntroductionComponent } from './modules/introduction/introduction.component';
+import { SignupComponent } from './modules/signup/signup.component';
+const routes: Routes = [
+  { path: '', component: IntroductionComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
