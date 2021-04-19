76fc4fddb38c82b78a87c56effbe2176f77e3548
subhakar-Prabhu <psubakar@gmail.com>
2021-04-19 17:01:43 +0530

    dashboard

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   formsBtn() {
-    alert('formsBtn');
+    this.router.navigate(['/signup']);
   }
 
   ngRxBtn() {
